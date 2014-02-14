2f525c03da8577cade9153f2ed980c090c86815b
Stephen Robinson <stephen@stephen-robinson.co.uk>
2014-02-14 18:29:54 +0000

    Added user class methods

--- api/src/main/java/user.java
+++ api/src/main/java/user.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+
 /**
  * Created by Stephen on 14/02/2014.
  */
 public class user {
+
+
+    // Register new user with LUFELF server
+
+    public ArrayList<String> register(ArrayList params){
+    }
+
+    // Login user to system and store login details locally
+
+    public ArrayList<String> login(ArrayList params){
+    }
+
+    // Get user details
+
+    public ArrayList<String> getDetails(ArrayList params){
+    }
+
+    // Delete user account on LUFELF Server
+
+    public ArrayList<String> delete(ArrayList params){
+    }
+
 }

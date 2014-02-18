76a60daa1058a8b62c999274ecb1c0dc0c34b96f
Stephen Robinson <stephen@stephen-robinson.co.uk>
2014-02-18 21:16:43 +0000

    Create V1 class - Provides frontend access to all api methods

---
+++ api/src/main/java/V1.java
@@ -0,0 +1,195 @@
+import java.util.Hashtable;
+
+/**
+ * Created by Stephen on 18/02/14.
+ */
+public class V1 {
+
+    public static void registerUser(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void loginUser(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getSingleUserDetails(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void deleteUser(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void createEvent(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getSingleEventDetails(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void deleteEvent(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getAllEvents(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getSocialEvents(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getUniversityEvents(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getOpenDayEvents(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getPublicEvents(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getPartyEvents(Hashtable params, NetworkCallback nc) {
+
+    }
+
+    public static void getStudyEvents(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void attendEvent(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void updateUserLocation(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void updateUserLocationStatus(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void sendMessage(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getSentMessages(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getReceivedMessages(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void createLocation(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getAllLocations(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getOtherLocations(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getPubs(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getRestaurants(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getLectureTheatres(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getSeminarRooms(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getCafes(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getTakeaways(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getCinemas(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getHalls(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getShops(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getLibraries(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void uploadProfilePicture(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getProfilePicture(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void uploadLocationPicture(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getLocationPicture(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void uploadEventPicture(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getEventPicture(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void addFriend(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void deleteFriend(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getFriendDetails(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getFriendLocation(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getFriendRequests(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void acceptFriendRequest(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void deleteFriendRequest(Hashtable params, NetworkCallback nc){
+
+    }
+
+    public static void getAllFriends(Hashtable params, NetworkCallback nc){
+
+    }
+}

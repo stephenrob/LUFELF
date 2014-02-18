3f3c0ea93e620f1ddaebf5df575adfba550ba5a8
Stephen Robinson <stephen@stephen-robinson.co.uk>
2014-02-18 23:07:20 +0000

    Added user details class for storing user details

---
+++ api/src/main/java/UserDetails.java
@@ -0,0 +1,31 @@
+/**
+ * Created by Stephen on 18/02/14.
+ */
+public class UserDetails {
+
+    int user_id = 0;
+    String name = null;
+    String lib_no = null;
+    String username = null;
+    String password = null;
+    String dob = null;
+    String type = null;
+    String description = null;
+    String avatar_url = null;
+    String location_status = null;
+    String access_level = null;
+    Boolean is_new = null;
+
+    public static final String USER_ID = "user_id";
+    public static final String NAME = "name";
+    public static final String USERNAME = "username";
+    public static final String LIBRARY_NUMBER = "lib_no";
+    public static final String PASSWORD = "password";
+    public static final String DATE_OF_BIRTH = "dob";
+    public static final String TYPE = "type";
+    public static final String DESCRIPTION = "description";
+    public static final String LOCATION_STATUS = "location_status";
+    public static final String ACCESS_LEVEL = "access_level";
+    public static final String IS_NEW = "is_new";
+
+}

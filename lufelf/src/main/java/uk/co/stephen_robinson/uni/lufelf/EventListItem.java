18813e5b73cb44619c9176718512dba55a812a63
gunmania0 <james@devine.eu>
2014-02-19 12:07:52 +0000

    added java doc

--- lufelf/src/main/java/uk/co/stephen_robinson/uni/lufelf/EventListItem.java
+++ lufelf/src/main/java/uk/co/stephen_robinson/uni/lufelf/EventListItem.java
@@ -4,103 +4,121 @@ import com.google.android.gms.maps.model.LatLng;
 
 /**
  * @author James
- *This models an event item in memory
+ *         This models an event item in memory
  */
 
 public class EventListItem {
 
-        private String title;
-        private int icon;
-        private String id;
-        private String eventName;
-        private String creator;
-        private LatLng location;
-        private String dateTime;
-        private String description;
+    private String title;
+    private int icon;
+    private String id;
+    private String eventName;
+    private String creator;
+    private LatLng location;
+    private String dateTime;
+    private String description;
 
-        /**
-         *Constructor for the eventlistitem
-         * @param title Title of the event
-         * @param icon  drawable resource of event
-         * @param id    event id
-         * @param eventName event name
-         * @param creator   the id of the person who created the event
-         * @param loc   LatLng of the event.
-         * @param dateTime  The date time of the event
-         * @param description   The description of the event.
-         */
-        public EventListItem(String title, int icon,String id, String eventName, String creator,LatLng loc,String dateTime,String description){
-            this.title=title;
-            this.icon=icon;
-            this.id=id;
-            this.eventName=eventName;
-            this.creator=creator;
-            this.location=loc;
-            this.dateTime=dateTime;
-            this.description=description;
-        }
+    private static int social=R.drawable.seminar;
+    private static int university=R.drawable.university;
+    private static int open_day=R.drawable.university;
+    private static int private_event=0;
+    private static int public_event=0;
+    private static int party=R.drawable.party;
+    private static int study=R.drawable.seminar_maybe;
 
-        /**
-         * get title of the event
-         * @return title of the EventListItem
-         */
-        public String getTitle(){
-            return this.title;
-        }
 
-        /**
-         * get the icon of the event
-         * @return icon resource id of the event
-         */
-        public int getIcon(){
-            return this.icon;
-        }
+    /**
+     * Constructor for the eventlistitem
+     *
+     * @param title       Title of the event
+     * @param iconID        drawable resource of event
+     * @param id          event id
+     * @param eventName   event name
+     * @param creator     the id of the person who created the event
+     * @param loc         LatLng of the event.
+     * @param dateTime    The date time of the event
+     * @param description The description of the event.
+     */
+    public EventListItem(String title, int iconID, String id, String eventName, String creator, LatLng loc, String dateTime, String description) {
+        this.title = title;
+        this.icon = iconID;
+        this.id = id;
+        this.eventName = eventName;
+        this.creator = creator;
+        this.location = loc;
+        this.dateTime = dateTime;
+        this.description = description;
+    }
 
-        /**
-         *get the id of the event
-         * @return the id of the event
-         */
-        public String getId(){
-            return this.id;
-        }
+    /**
+     * get title of the event
+     *
+     * @return title of the EventListItem
+     */
+    public String getTitle() {
+        return this.title;
+    }
 
-        /**
-         * get the location of the event
-         * @return the location of the event
-         */
-        public LatLng getLocation() {
-            return location;
-        }
+    /**
+     * get the icon of the event
+     *
+     * @return icon resource id of the event
+     */
+    public int getIcon() {
+        return this.icon;
+    }
 
-        /**
-         * get the creator of the event
-         * @return id of the creator
-         */
-        public String getCreator() {
-            return creator;
-        }
+    /**
+     * get the id of the event
+     *
+     * @return the id of the event
+     */
+    public String getId() {
+        return this.id;
+    }
 
-        /**
-         * get the date and time of event
-         * @return the date and time of event
-         */
-        public String getDateTime() {
-            return dateTime;
-        }
+    /**
+     * get the location of the event
+     *
+     * @return the location of the event
+     */
+    public LatLng getLocation() {
+        return location;
+    }
 
-        /**
-         * get the description of the event
-         * @return the description
-         */
-        public String getDescription() {
-            return description;
-        }
+    /**
+     * get the creator of the event
+     *
+     * @return id of the creator
+     */
+    public String getCreator() {
+        return creator;
+    }
 
-        /**
-         * get the event name
-         * @return event name
-         */
-        public String getEventName() {
-            return eventName;
-        }
+    /**
+     * get the date and time of event
+     *
+     * @return the date and time of event
+     */
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    /**
+     * get the description of the event
+     *
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * get the event name
+     *
+     * @return event name
+     */
+    public String getEventName() {
+        return eventName;
+    }
 }

94a69a065141d0afc9225ff4b7722d186c6ae049
Stephen Robinson <stephen@stephen-robinson.co.uk>
2014-02-20 15:57:46 +0000

    fixed generation of http client

--- api/src/main/java/uk/co/stephen_robinson/uni/lufelf/api/Network/Network.java
+++ api/src/main/java/uk/co/stephen_robinson/uni/lufelf/api/Network/Network.java
@@ -9,6 +9,7 @@ import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.util.Hashtable;
 import java.util.List;
@@ -100,6 +101,7 @@ public class Network extends AsyncTask<List<NameValuePair>, Integer, Hashtable>{
     public Network(NetworkCallback nc, Script script){
         this.networkCallback = nc;
         this.serverScript = script;
+        serverClient = new DefaultHttpClient();
     }
 
     // Accepts List<namevaluepair> for server parameters

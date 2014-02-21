3f2dedbb20d33c0ee8ce7550c8aa911fb5ef0249
Stephen Robinson <stephen@stephen-robinson.co.uk>
2014-02-21 03:41:05 +0000

    updated network - single callback

--- api/src/main/java/uk/co/stephen_robinson/uni/lufelf/api/network/callbacks/Single.java
+++ api/src/main/java/uk/co/stephen_robinson/uni/lufelf/api/network/callbacks/Single.java
@@ -1,7 +1,10 @@
 package uk.co.stephen_robinson.uni.lufelf.api.network.callbacks;
 
+import java.util.Hashtable;
+
 /**
  * Created by Stephen on 21/02/14.
  */
-public class Single {
+public interface Single {
+    void results(Hashtable result);
 }
